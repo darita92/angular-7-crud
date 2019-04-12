e640dd02a7791bd13e4cd6be44effdc38bcf0c9d
Daniel Arita <darita@HN-LNOHN0211m.loe.corp>
2019-04-12 15:50:55 -0600

    update url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiKey: 'fe1d1cf9-b035-4025-98a3-d3f192d9e8e0',
-  API_BASE_URL: 'https://kambda-test-api.herokuapp.com/'
+  API_BASE_URL: 'https://kambda-test-api.herokuapp.com'
 };
